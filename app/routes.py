4fdad1751d50c5dbd4142f6f1c522ae1eef9ede7
David Medinets <david.medinets@gmail.com>
2018-02-09 10:43:12 -0500

    assign uuid as needed

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,24 @@
+from calendar import monthrange
+from flask import render_template, redirect, url_for
 from app import app
+
+import calendar
+import datetime
+import uuid
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return redirect(url_for('indexWithHandle', handle=str(uuid.uuid4())))
+
+@app.route('/<handle>')
+def indexWithHandle(handle):
+    now = datetime.datetime.now()
+    (weekday, daysInMonth) = monthrange(2018, 2)
+    print(now.year, now.month, daysInMonth)
+    return render_template(
+        'index.html', 
+        handle=handle,
+        year=now.year,
+        month=now.month,
+        monthName=calendar.month_name[now.month],
+        daysInMonth=daysInMonth)
