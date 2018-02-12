803779ddf78befb5915b8679631ca8e4cb8cb704
David Medinets <david.medinets@gmail.com>
2018-02-12 09:27:30 -0500

    morning, evening, and target lines

--- app/models.py
+++ app/models.py
@@ -22,35 +22,25 @@ class BloodSugarMonth(db.Model):
             oEvening = oEvening + 'evening{}=({}) '.format(n, self.__getattribute__('evening'+str(n)))
         return '<BloodSugarMonth id({}) uuid({} year({}) month({}) {} {})>'.format(self.id, self.uuid, self.year, self.month, oMorning, oEvening)
 
-    def populateMonthForm(handle, year, month):
-        (weekday, daysInMonth) = monthrange(year, month)
-        form = MonthForm()
-        form.handle.data = handle
-        form.year.data = year
-        form.month.data = month
-        form.daysInMonth.data = daysInMonth
-
-        # see if database record exists
-        record = db.session.query(BloodSugarMonth)\
-            .filter(BloodSugarMonth.uuid == handle)\
-            .filter(BloodSugarMonth.year == year)\
-            .filter(BloodSugarMonth.month == month)\
-            .first()
-
-        if record:
-            for n in range(1, 32):
-                mValue = record.__getattribute__('morning'+str(n))
-                if mValue != 0 and mValue != None:
-                    for field in form:
-                        if field.name == 'morning' + str(n):
-                            field.data = mValue
-                eValue = record.__getattribute__('evening'+str(n))
-                if eValue != 0 and eValue != None:
-                    for field in form:
-                        if field.name == 'evening' + str(n):
-                            field.data = eValue
-
-        return form
+    def getGraphInfo(self):
+        minimum = 9999
+        maximum = 0
+        for n in range(1, 32):
+            mValue = self.__getattribute__('morning'+str(n))
+            eValue = self.__getattribute__('evening'+str(n))
+            if mValue:
+                if mValue < minimum:
+                    minimum = mValue
+                if mValue > maximum:
+                    maximum = mValue
+            if eValue:
+                if eValue < minimum:
+                    minimum = eValue
+                if eValue > maximum:
+                    maximum = eValue
+        minimum = minimum - 10
+        maximum = maximum + 10
+        return minimum, maximum
 
     def save(form):
 
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,42 @@
 from calendar import monthrange
 from flask import render_template, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import MonthForm
 from app.models import BloodSugarMonth
 
 import calendar
 import datetime
 import uuid
+
+def populateMonthForm(handle, year, month):
+    (weekday, daysInMonth) = monthrange(year, month)
+    form = MonthForm()
+    form.handle.data = handle
+    form.year.data = year
+    form.month.data = month
+    form.daysInMonth.data = daysInMonth
+
+    # see if database record exists
+    record = db.session.query(BloodSugarMonth)\
+        .filter(BloodSugarMonth.uuid == handle)\
+        .filter(BloodSugarMonth.year == year)\
+        .filter(BloodSugarMonth.month == month)\
+        .first()
+
+    if record:
+        for n in range(1, 32):
+            mValue = record.__getattribute__('morning'+str(n))
+            if mValue != 0 and mValue != None:
+                for field in form:
+                    if field.name == 'morning' + str(n):
+                        field.data = mValue
+            eValue = record.__getattribute__('evening'+str(n))
+            if eValue != 0 and eValue != None:
+                for field in form:
+                    if field.name == 'evening' + str(n):
+                        field.data = eValue
+
+    return form, record
 
 @app.route('/')
 def index():
@@ -17,20 +47,28 @@ def index():
 @app.route('/<handle>')
 def indexWithHandle(handle):
     now = datetime.datetime.now()
-    form = BloodSugarMonth.populateMonthForm(handle, now.year, now.month)
+    form, record = populateMonthForm(handle, now.year, now.month)
+    if record:
+        minimum, maximum = record.getGraphInfo()
     return render_template(
         'index.html',
         monthName=calendar.month_name[now.month],
         daysInMonth=int(form.daysInMonth.data),
+        minimum=minimum,
+        maximum=maximum,
         form=form)
 
 @app.route('/<handle>/<int:year>/<int:month>')
 def indexWithHandleAndDate(handle, year, month):
-    form = BloodSugarMonth.populateMonthForm(handle, year, month)
+    form, record = populateMonthForm(handle, year, month)
+    if record:
+        minimum, maximum = record.getGraphInfo()
     return render_template(
         'index.html',
         monthName=calendar.month_name[month],
         daysInMonth=int(form.daysInMonth.data),
+        minimum=minimum,
+        maximum=maximum,
         form=form)
 
 @app.route('/process-form', methods=['POST'])
