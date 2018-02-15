bca930dc4c679670d61822b07e97799d13a10c6f
David Medinets <david.medinets@gmail.com>
2018-02-15 15:38:17 -0500

    remove unused DayForm class

--- app/forms.py
+++ app/forms.py
@@ -2,10 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import FieldList, HiddenField, IntegerField, StringField, SubmitField, validators
 from wtforms.validators import DataRequired, UUID
 
-class DayForm(FlaskForm):
-    morning = IntegerField('morning')
-    evening = IntegerField('evening')
-
 class MonthForm(FlaskForm):
     handle = HiddenField('handle', validators=[DataRequired(), UUID()])
     month = HiddenField('month', validators=[DataRequired()])
